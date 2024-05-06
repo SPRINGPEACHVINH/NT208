610f3886e8a60da7947831c277db9770663a3b6c
SPRINGPEACHVINH <22521666@gm.uit.edu.vn>
2024-05-06 21:04:41 +0700

    add antd, redux

--- frontend-ticketx88/src/index.js
+++ frontend-ticketx88/src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom";
 import "./styles/index.css";
 import App from "./views/App";
 import reportWebVitals from "./reportWebVitals";
+import { store } from './redux/store'
+import { Provider } from 'react-redux'
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
-  document.getElementById("root")
 );
 reportWebVitals();
