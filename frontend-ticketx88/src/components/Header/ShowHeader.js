9ec7cc12f973f755f4b031dedbd636f110e95560
Neit272 <coldboykiller@gmail.com>
2024-05-17 05:35:38 +0700

    show recommended event as searching

--- frontend-ticketx88/src/components/Header/ShowHeader.js
+++ frontend-ticketx88/src/components/Header/ShowHeader.js
@@ -1,8 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import "../../styles/Header.css";
 
 const ShowHeader = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [results, setResults] = useState([]);
+  const [isTyping, setIsTyping] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (isTyping && searchTerm !== "") {
+        fetchResults();
+      }
+    }, 5000); // 5000ms delay
+
+    return () => clearTimeout(timer);
+  }, [searchTerm, isTyping]);
+
+  const fetchResults = async () => {
+    const response = await fetch(
+      `http://localhost:8888/api/event/search?q=${searchTerm}`
+    );
+    const data = await response.json();
+    setResults(data);
+  };
+
+  const handleInputChange = (event) => {
+    setSearchTerm(event.target.value);
+    setIsTyping(true);
+  };
+
   return (
     <header className="header">
       <div className="logo">TicketX88</div>
@@ -13,7 +40,16 @@ const ShowHeader = () => {
             id="search-input"
             placeholder="Bạn tìm gì hôm nay?"
             autoComplete="off"
+            value={searchTerm}
+            onChange={handleInputChange}
           />
+          {results.length > 0 && (
+            <ul>
+              {results.map((result, index) => (
+                <li key={index}>{result.EventName}</li>
+              ))}
+            </ul>
+          )}
           <button id="search-btn">Tìm kiếm</button>
         </div>
         <div className="actions">
