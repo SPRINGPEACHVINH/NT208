f61ca68ce0fa959afe96038fcb5e392b0d7b7311
Neit272 <coldboykiller@gmail.com>
2024-05-17 06:00:07 +0700

    update server side to work when host locally using cors

--- src/routes/EventRouter.js
+++ src/routes/EventRouter.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const cors = require("cors");
+
 const router = express.Router();
+
+router.use(cors());
+
 const EventController = require("../controllers/EventController");
 
 router.get("/search", EventController.search);
