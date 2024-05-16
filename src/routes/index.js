a3d0957f96f7886224cb8c5c94340cb30e1054d5
Neit272 <coldboykiller@gmail.com>
2024-05-17 02:53:39 +0700

    recode after bug fix

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,9 @@
-const UserRouter = require('./UserRouter')
+const UserRouter = require("./UserRouter");
+const EventRouter = require("./EventRouter");
 
 const routes = (app) => {
-    app.use('/api/user', UserRouter)
-}
+  app.use("/api/user", UserRouter);
+  app.use("/api/event", EventRouter);
+};
 
 module.exports = routes;
