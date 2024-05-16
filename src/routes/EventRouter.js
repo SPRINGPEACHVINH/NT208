18cbf76d7b734f2450e2386c38e0679af0015c7a
SPRINGPEACHVINH <22521666@gm.uit.edu.vn>
2024-05-17 04:07:07 +0700

    commit

--- src/routes/EventRouter.js
+++ src/routes/EventRouter.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-
 const EventController = require("../controllers/EventController");
 
 router.get("/search", EventController.search);
