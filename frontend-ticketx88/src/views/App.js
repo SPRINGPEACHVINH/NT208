57eee15e7d3f82ed914bd130938813050806cc43
Neit272 <coldboykiller@gmail.com>
2024-05-18 10:25:50 +0700

    change axios to fetch

--- frontend-ticketx88/src/views/App.js
+++ frontend-ticketx88/src/views/App.js
@@ -1,19 +1,18 @@
 import React, { Fragment, useEffect } from "react";
-import axios from "axios";
 import { routes } from "../routes";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import DefaultComponent from "../components/DefaultComponent/DefaultComponent";
 
 function App() {
-
   useEffect(() => {
-    fetchApi()
-  }, [])
+    fetchApi();
+  }, []);
 
   const fetchApi = async () => {
-    const res = axios.get(`${process.env.REACT_API_URL_BACKEND}/user/getAll`)
-    console.log('res', res)
-  }
+    const res = await fetch(`${process.env.REACT_API_URL_BACKEND}/user/getAll`);
+    const data = await res.json();
+    console.log("res", data);
+  };
   return (
     <div>
       <Router>
@@ -22,12 +21,15 @@ function App() {
             const Page = route.page;
             const Layout = route.isShowHeader ? DefaultComponent : Fragment;
             return (
-              <Route path={route.path} element={
-                <Layout>
-                  <Page />
-                </Layout>
-              } />
-            )
+              <Route
+                path={route.path}
+                element={
+                  <Layout>
+                    <Page />
+                  </Layout>
+                }
+              />
+            );
           })}
         </Routes>
       </Router>
