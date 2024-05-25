76dc77d549e810b7cb1990966201a678d7b0d361
SPRINGPEACHVINH <22521666@gm.uit.edu.vn>
2024-05-25 14:41:10 +0700

    doing oauth2

--- src/index.js
+++ src/index.js
@@ -3,36 +3,26 @@ const dotenv = require("dotenv");
 const { default: mongoose } = require("mongoose");
 const routes = require("./routes");
 const cors = require("cors");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
 const bodyParser = require("body-parser");
 dotenv.config();
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-// const uri = "mongodb+srv://mongodbuser:TicketX.88@ticketx88.itndfbh.mongodb.net/?retryWrites=true&w=majority&appName=TicketX88";
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   }
-// });
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
 
 const app = express();
 const port = process.env.PORT || 8881;
 
-app.use(cors());
+app.use(
+  cookieSession({
+    name: "session",
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(cors({
+  origin: "http://localhost:8881",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true,
+}));
 app.use(bodyParser.json());
 
 routes(app);
---
+++ src/routes/oauth.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const dotenv = require("dotenv");
+dotenv.config();
+const { Oauth2Client } = require("google-auth-library");
+
+
+
+module.exports = router;
