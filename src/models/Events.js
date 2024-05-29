defd67c22e8188924efdd9b7d09bc9cf3bb8084f
SPRINGPEACHVINH <22521666@gm.uit.edu.vn>
2024-05-30 01:40:40 +0700

    update dtb

--- src/models/Events.js
+++ src/models/Events.js
@@ -8,9 +8,10 @@ const eventSchema = new mongoose.Schema(
     EventLocation: { type: String, required: true },
     EventCategory: { type: String, required: true },
     TicketPrice: { type: Number, required: true },
+    TicketId: { type: mongoose.Schema.Types.ObjectId, ref: "Ticket", required: true},
     Picture_event: { type: String, required: true },
     Logo_event: { type: String, required: false },
-    Btc: { type: mongoose.Schema.Types.ObjectId, ref: "Btc", required: false },
+    Btc: { type: mongoose.Schema.Types.ObjectId, ref: "Btc", required: true, unique: true},
   },
   {
     timestamps: true,
