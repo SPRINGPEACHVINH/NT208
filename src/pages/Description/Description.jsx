52d97e385ef98be1fc2ec440a0e04c82066d478c
Neit272 <coldboykiller@gmail.com>
2024-05-30 17:42:54 +0700

    user has to login to buy ticket

--- src/pages/Description/Description.jsx
+++ src/pages/Description/Description.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
-import { useSelector } from "react-redux";
 import ShowDescription from "./ShowDescription";
 import TicketInfo from "../../components/TicketInfo";
 import ShowBanner from "../../components/Banner/ShowBanner";
@@ -12,8 +11,7 @@ const ShowPage = () => {
   const [event, setEvent] = useState(null);
   const [user, setUser] = useState(null);
 
-  // Access the username from the Redux store
-  const username = useSelector(state => state.username);
+const username = localStorage.getItem("username");
 
   useEffect(() => {
     const fetchEvent = async () => {
@@ -29,6 +27,11 @@ const ShowPage = () => {
     };
 
     const fetchUser = async () => {
+      if (!username) {
+        setUser(null);
+        return;
+      }
+
       const response = await fetch(
         `http://localhost:8881/api/user/get-details/${username}`
       );
@@ -36,13 +39,14 @@ const ShowPage = () => {
         console.error(`Failed to fetch user: ${response.statusText}`);
         return;
       }
+      console.log(response);
       const data = await response.json();
       setUser(data);
     };
 
     fetchEvent();
     fetchUser();
-  }, [EventId, username]); // Add username to the dependency array
+  }, [EventId, username]);
 
   return (
     <div className="layout">
@@ -51,7 +55,7 @@ const ShowPage = () => {
           <ShowBanner event={event} />
           <div className="contents">
             <ShowDescription event={event} />
-            <TicketInfo event={event} user={user} /> {/* Pass the user to TicketInfo */}
+            <TicketInfo event={event} user={user} />
           </div>
         </>
       )}
