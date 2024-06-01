0606f88c4b0df1102997ef761ee137e018107bac
SPRINGPEACHVINH <22521666@gm.uit.edu.vn>
2024-06-01 22:32:22 +0700

    merge

--- src/routes/index.js
+++
@@ -1,9 +0,0 @@
-const UserRouter = require("./UserRouter");
-const EventRouter = require("./EventRouter");
-
-const routes = (app) => {
-  app.use("/api/user", UserRouter);
-  app.use("/api/event", EventRouter);
-};
-
-module.exports = routes;
