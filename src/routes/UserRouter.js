035915886714cbe807833f90e1fd5fbe1a12d033
Le Phat Anh Tuan <lpatuan27@gmail.com>
2024-06-08 00:00:23 +0700

    sao sua cai details khong noi

--- src/routes/UserRouter.js
+++ src/routes/UserRouter.js
@@ -9,6 +9,7 @@ router.put("/update-user/:id", userController.UpdateUser);
 router.delete("/delete-user/:id", userController.DeleteUser);
 router.get("/getAll", userController.GetAllUser);
 router.get("/get-details/:UserName", userController.GetDetailsUser);
+router.get("/get-id/:UserName", userController.GetIdUser);
 router.get("/get-events/:UserName", userController.GetEvents);
 router.post("/refresh-token", userController.RefreshToken);
 router.post("/forgot-password", userController.ForgotPassword);
